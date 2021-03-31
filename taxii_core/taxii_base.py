6f1f2b56592648b888825f530ffbdd976d5cf482
John Omernik <darkness@cybermac.local>
2021-03-31 10:49:46 -0500

    Updates

--- taxii_core/taxii_base.py
+++ taxii_core/taxii_base.py
@@ -80,6 +80,9 @@ class Taxii(Integration):
         if inst is not None:
             if inst['options'].get('useproxy', 0) == 1:
                 proxystr = self.get_proxy_str(instance)
+                if self.debug:
+                    print("Proxy String: %s" % proxystr)
+
                 prox_pass = self.get_proxy_pass(proxystr, instance)
                 proxurl = proxystr.replace("@", ":" + prox_pass + "@")
                 proxies = {'http': proxurl, 'https': proxurl}
